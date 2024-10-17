010c9d4e0a90954d3badf529763d8686ebd38e9f
Kunal Singh <kunalsin9h@gmail.com>
2024-10-17 20:54:32 +0530

    wip: test rules page in ui

--- ui/src/AddRule.tsx
+++ ui/src/AddRule.tsx
@@ -43,8 +43,13 @@ export default function AddRule() {
             value={name}
             onChange={handleNameChange}
             className="bg-gray-700 text-white px-4 py-2 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 resize-none w-full"
+            onKeyDown={(e) => {
+              if (e.code === 'Space') {
+                e.preventDefault();
+              }
+            }}
           />
-        </div>
+          </div>
         <div>
           <label htmlFor="description" className="text-white font-bold">Description:</label>
           <input
--- ui/src/TestRule.tsx
+++ ui/src/TestRule.tsx
@@ -1,34 +1,42 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export default function TestRule() {
-  const [rule, setRule] = useState('');
+  const [rules, setRules] = useState([]);
+  const [error, setError] = useState(false);
 
-  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setRule(event.target.value);
-  };
+  useEffect(() => {
+    const fetchRules = async () => {
+      try {
+        const response = await fetch('/rules');
+        
+        if (response.status !== 200) {
+          setError(true);
+          console.error('Failed to fetch rules:', response.status);
+        } else {
+          const data = await response.json();
+          setRules(data);
+        }
+      } catch (error) {
+        console.error('Error fetching rules:', error);
+      }
+    };
 
-  const handleAddRule = () => {
-    // Implement logic to add the rule
-    console.log('Adding rule:', rule);
-    setRule('');
-  };
+    fetchRules();
+  }, []);
   return (
-    <div className="flex flex-col gap-4 w-full">
-      <label htmlFor="rule" className="text-white font-bold">Rule:</label>
-      <textarea
-        id="rule"
-        value={rule}
-        // @ts-expect-error
-        onChange={handleInputChange}
-        className="bg-gray-700 text-white px-4 py-2 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 resize-none w-full"
-        style={{ width: '100%', height: "200px" }}
-      />
-      <button
-        onClick={handleAddRule}
-        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 flex items-center justify-center"
-      >
-        Add
-      </button>
+    <div>
+      <h1 className="text-white font-bold">All Rules</h1>
+      {error && <p className="text-red-500">Error fetching rules. Oops, something went wrong!</p>}
+      <ul className="flex flex-col gap-4">
+        {rules.map((rule, index) => (
+          <li key={index} className="bg-gray-700 text-white px-4 py-2 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 cursor-pointer hover:bg-gray-600 flex flex-col">
+            {/*  @ts-ignore*/}
+            <span className="font-semibold">{rule.Name}</span>
+            {/*  @ts-ignore*/}
+            <span className="text-xs text-gray-400">{rule.Description}</span>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
